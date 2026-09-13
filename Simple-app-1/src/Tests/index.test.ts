49841dba57fcb63fa6cf0239ecff630dc76f016d
Amjadkhan9756 <amjadkhann.tech@gmail.com>
2026-09-13 10:45:28 +0530

    starting setup of testing

---
+++ Simple-app-1/src/Tests/index.test.ts
@@ -0,0 +1,21 @@
+import {describe,expect,it} from "@jest/globals"
+import sum, { multiply } from "../index.js";
+
+
+describe("testing fo rsum function ",()=>{
+    it("should be it correct ",()=>{
+        const finaleAnwser=sum(1,2);
+
+        expect(finaleAnwser).toBe(3);
+    });
+
+    
+})
+
+describe("testing for multiple function ",()=>{
+    it("should it be correct ",()=>{
+        const finaleAnwser = multiply(2,3);
+
+        expect(finaleAnwser).toBe(6);
+    })
+})
---
+++ Simple-app-1/src/index.ts
@@ -0,0 +1,12 @@
+
+
+function sum(a:number,b:number){
+    return a+b
+}
+export default sum;
+
+
+export function multiply(a:number,b:number){
+    return a*b;
+}
+
